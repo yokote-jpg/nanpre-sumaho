6124e4ba2482110ac12e0364b510fefcf9cd506e
yokote-jpg <yokote@ivy.ocn.ne.jp>
2026-05-03 17:18:59 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'nanpre-v2';
+const FILES = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
